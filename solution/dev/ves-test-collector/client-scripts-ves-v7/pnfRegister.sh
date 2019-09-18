f58b3e1c0d88c9c403cbbef28cb36fa2f1b6bf86
demx8as6 <martin.skorupski@highstreet-technologies.com>
2019-09-18 08:53:26 +0200

    Intial version for solution/dev
    The purpose of this directory is to provide an setups for
    O-RAN component developers, who need to implement the O1 interface provider.
    
    The SMO (Service Management and Orchestration) basically contains an
    OpenDaylight based NetConf Client, which is also used by the ONAP
    project an a VES Test Collector.
    
    IssueID: OAM-10
    Change-Id: I432b16a907bf9c2fc2294d45eb4404beeb2e6796
    Signed-off-by: demx8as6 <martin.skorupski@highstreet-technologies.com>

---
+++ solution/dev/ves-test-collector/client-scripts-ves-v7/pnfRegister.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+################################################################################
+#
+# Copyright 2019 highstreet technologies GmbH and others
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+################################################################################
+# Script to send an VES Message Event to DCAE
+
+. config;
+      pnfType=${1,,};
+      domain="pnfRegistration";
+
+declare -A mapping=(
+    [controllerName]=$(hostname --fqdn)
+    [domain]=$domain
+    [eventId]="${pnfIdByType[$pnfType]}_${modelByType[$pnfType]}"
+    [eventTime]=${eventTime}
+    [eventType]=EventType5G
+    [pnfId]=${pnfIdByType[$pnfType]}
+    [type]=${pnfType^^}
+    [model]=${modelByType[$pnfType]}
+    [oamIp]=${oamIpByType[$pnfType]}
+    [macAddress]=02:42:f7:d4:62:ce
+    [oamIpV6]=0:0:0:0:0:ffff:a0a:0${oamIpByType[$pnfType]:(-2)}
+    [vendor]=${vendorsByType[$pnfType]^^}
+    [timestamp]=${timestamp}
+    [eventId]="${pnfIdByType[$pnfType]}_${modelByType[$pnfType]}"
+    [eventTime]=${eventTime}
+    [eventType]=EventType5G
+)
+
+echo "################################################################################";
+echo "# send PNF registration";
+echo;
+for key in "${!mapping[@]}"
+do
+  #label=${${"$spaces$i"}:(-14)};
+  label=$spaces$key;
+  label=${label:(-16)};
+  echo "$label: ${mapping[$key]}";
+  if [ $key = "timestamp" ]; then
+      sequence="$sequence s/\"@$key@\"/${mapping[$key]}/g; "
+  else
+      sequence="$sequence s/@$key@/${mapping[$key]}/g; "
+  fi  
+done
+echo;
+
+body="./json/examples/${pnfType^^}-${domain}.json"
+sed -e "$sequence" ./json/templates/$domain.json > $body;
+
+curl -i -k -u $basicAuthVes -X POST -d @${body} --header "Content-Type: application/json" $urlVes
---
+++ solution/dev/ves-test-collector/client-scripts-ves-v7/send15minPm.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+################################################################################
+#
+# Copyright 2019 highstreet technologies GmbH and others
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+################################################################################
+# Script to send an VES Message Event to DCAE
+   
+. config;
+             pnfType=${1,,};
+              domain="measurement";
+   collectionEndTime=$(( $timeInS - $(($timeInS % 900))));
+ collectionStartTime=$(( collectionEndTime - 900 ));
+         granularity="PM15min";
+ 
+declare -A mapping=(
+    [domain]=$domain
+    [controllerName]=$(hostname --fqdn)
+    [pnfId]=${pnfIdByType[$pnfType]}
+    [granularity]=$granularity
+    [eventId]="${pnfIdByType[$pnfType]}_${collectionEndTime}_${granularity}"
+    [eventType]=${eventType}
+    [type]=${pnfType^^}
+    [interface]=${interfaceByType[$pnfType]}
+    [timestamp]=${timestamp}
+    [eventTime]=${eventTime}
+    [collectionStartTime]=${collectionStartTime}000
+    [collectionEndTime]=${collectionEndTime}000
+    [intervalStartTime]=$(date -u -R -d @$collectionStartTime )
+    [intervalEndTime]=$(date -u -R -d @$collectionEndTime )
+    [vendor]=${vendorsByType[$pnfType]^^}
+    [model]=${modelByType[$pnfType]}
+)
+
+echo "################################################################################";
+echo "# send 15min performance values";
+echo
+for key in "${!mapping[@]}"
+do
+  label=$spaces$key;
+  label=${label:(-20)};
+  echo "$label: ${mapping[$key]}";
+  if [ $key = "collectionStartTime" ] || [ $key = "collectionEndTime" ]; then
+      sequence="$sequence s/\"@$key@\"/${mapping[$key]}/g; "
+  else
+      sequence="$sequence s/@$key@/${mapping[$key]}/g; "
+  fi  
+done
+echo;
+
+body=./json/examples/${pnfType^^}-${domain}.json
+sed -e "$sequence" ./json/templates/$domain.json > $body
+
+curl -i -k -u $basicAuthVes -X POST -d  @${body} --header "Content-Type: application/json" $urlVes
