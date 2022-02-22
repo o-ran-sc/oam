ac7793b2d2a0fa856fbd9c003fed41282f7af81c
demx8as6 <martin.skorupski@highstreet-technologies.com>
2022-02-22 12:53:57 +0100

    Create a script to generate a Topology
    - add SVG representation for a topology node as O-cloud IF provider
    
    Issue-ID: OAM-249
    Change-Id: I58dc4dcda9301d601373df64e3bf0bf8d8f98ee8
    Signed-off-by: demx8as6 <martin.skorupski@highstreet-technologies.com>

---
+++ code/network-topology-instance-generator/model/python/svg/o_cloud.py
@@ -0,0 +1,34 @@
+# Copyright 2022 highstreet technologies GmbH
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#!/usr/bin/python
+"""
+Module containing a class representing an SVG Element as O-RAN Distributed Unit
+"""
+from model.python.svg.connection_edge_point import ConnectionEdgePoint
+from model.python.svg.node import Node
+
+
+class OCloud(Node):
+    """
+    Class representing an SVG Element object as O-RAN Distributed Unit
+    """
+
+    def width(self) -> int:
+        """
+        Getter for width of the SVG Element
+        :return Width in pixel
+        """
+        self.__width = 16*ConnectionEdgePoint.width(self)
+        return self.__width
