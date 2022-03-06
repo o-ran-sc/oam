3c9f12c84ae3cb8cf0e20e35534680b96763b18b
demx8as6 <martin.skorupski@highstreet-technologies.com>
2022-03-06 14:04:18 +0100

    ADD N1, N2 N3 interfacing to topology generation
    - new svg/Amf class created for SVG representation of a
      5G Core Access and Mobility Management Function
    
    Issue-Id: OAM-256
    Change-Id: Iad977d20fb1969d3d308b1af7848c90e29a7050e
    Signed-off-by: demx8as6 <martin.skorupski@highstreet-technologies.com>

---
+++ code/network-topology-instance-generator/model/python/svg/amf.py
@@ -0,0 +1,36 @@
+# Copyright 2022 highstreet technologies GmbH
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#!/usr/bin/python
+"""
+Module containing a class representing an SVG Element as 5G-Core Access and
+Mobility Management Function
+"""
+from model.python.svg.connection_edge_point import ConnectionEdgePoint
+from model.python.svg.node import Node
+
+
+class Amf(Node):
+    """
+    Class representing an SVG Element object as 5G-Core Access and
+    Mobility Management Function
+    """
+
+    def width(self) -> int:
+        """
+        Getter for width of the SVG Element
+        :return Width in pixel
+        """
+        self.__width = 2*ConnectionEdgePoint.width(self)
+        return self.__width
