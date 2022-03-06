70ce889efa03cbb1ab95cc153e19bf2db4b32fce
demx8as6 <martin.skorupski@highstreet-technologies.com>
2022-03-06 14:02:53 +0100

    ADD N1, N2 N3 interfacing to topology generation
    - new TapiNodeUpf class created for representation of a
      5G Core User Plane Function
    
    Issue-Id: OAM-256
    Change-Id: I4720ca34262e4a9830aae33036574675b39cbbc7
    Signed-off-by: demx8as6 <martin.skorupski@highstreet-technologies.com>

---
+++ code/network-topology-instance-generator/model/python/tapi_node_upf.py
@@ -0,0 +1,41 @@
+# Copyright 2022 highstreet technologies GmbH
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#!/usr/bin/python
+"""
+Module containing a class representing a 3GPP 5G Core User Plane Function as TAPI Node.
+"""
+from model.python.tapi_node import TapiNode
+from model.python.tapi_node_edge_point import TapiNodeEdgePoint
+
+
+class TapiNodeUpf(TapiNode):
+    """
+    Class representing a 3GPP 5G Core User Plane Function as TAPI Node.
+    """
+
+    # constructor
+    def __init__(self, parent, config):
+        super().__init__(parent, config)
+
+        super().width( (1 + 1) * (2*self.FONTSIZE) ) # 1x nep
+
+        # add N3 Provider interface
+        nep_configuration = {
+            "parent": self.identifier(),
+            "nodeEdgePoint": {
+                "interface": "N3", "cep":[{"protocol": "NAS", "role": "provider"}]
+            }
+        }
+        self.add(TapiNodeEdgePoint(nep_configuration))
