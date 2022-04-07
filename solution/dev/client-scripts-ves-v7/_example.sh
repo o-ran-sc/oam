18e398bed2bf9085cf389a8b04c842880afab2fb
demx8as6 <martin.skorupski@highstreet-technologies.com>
2022-04-07 17:09:32 +0200

    Create more VES stndDefined Examples
    Nessesary updates on the examlpes to facilitat the usage of the script.
    
    Issue-ID: OAM-263
    Change-Id: Iba3e9cdfebd67df4bfd224f82d5816dcb18052f5
    Signed-off-by: demx8as6 <martin.skorupski@highstreet-technologies.com>

--- solution/dev/client-scripts-ves-v7/_example.sh
+++ solution/dev/client-scripts-ves-v7/_example.sh
@@ -6,9 +6,9 @@
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     https://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -24,13 +24,13 @@
 
 ################################################################################
 # send pnf registration event
-./pnfRegister.sh 1234 
-./pnfRegister.sh FYNG 
-./pnfRegister.sh R2D2 
-./pnfRegister.sh 7DEV 
-./pnfRegister.sh nSky 
-./pnfRegister.sh 1OSF 
-./pnfRegister.sh SDNR 
+./pnfRegister.sh 1234
+./pnfRegister.sh FYNG
+./pnfRegister.sh R2D2
+./pnfRegister.sh 7DEV
+./pnfRegister.sh nSky
+./pnfRegister.sh 1OSF
+./pnfRegister.sh SDNR
 
 ################################################################################
 # raise fault
@@ -41,6 +41,16 @@
 ./sendFault.sh nSky LossOfSignalAlarm CRITICAL
 ./sendFault.sh 1OSF HAAMRunningInLowerModulation MAJOR
 ./sendFault.sh SDNR connectionLossNe MAJOR
+
+################################################################################
+# raise stndDefined NotifyNewAlarm
+./sendStndDefinedNotifyNewAlarm.sh 1234 lossOfSignal CRITICAL
+./sendStndDefinedNotifyNewAlarm.sh FYNG TCA MAJOR
+./sendStndDefinedNotifyNewAlarm.sh R2D2 TCA MINOR
+./sendStndDefinedNotifyNewAlarm.sh 7DEV signalIsLost CRITICAL
+./sendStndDefinedNotifyNewAlarm.sh nSky LossOfSignalAlarm CRITICAL
+./sendStndDefinedNotifyNewAlarm.sh 1OSF HAAMRunningInLowerModulation MAJOR
+./sendStndDefinedNotifyNewAlarm.sh SDNR connectionLossNe MAJOR
 
 ################################################################################
 # clear fault
