7204cd289201cd33e0faf5fde60b2f1a2608bf2d
Martin Skorupski <martin.skorupski@highstreet-technologies.com>
2023-04-01 17:42:23 +0200

    Script which adds yang-revision to yang-file-name
    - add file
    - move tmux-logging.py to helper dir
    
    Issue-ID: OAM-323
    Change-Id: I299b7d04c5b9cd1367f71dd78ea7cfe9901507ca
    Signed-off-by: Martin Skorupski <martin.skorupski@highstreet-technologies.com>

---
+++ code/helpers/yang_file_name_with_revision.py
@@ -0,0 +1,43 @@
+import os
+import re
+import time
+
+# Specify the directory containing the files
+directory_path = "."
+
+# Define the regular expression to search for revision dates
+revision_date_regex = r"revision\s*\"{0,1}(\d{4}-\d{2}-\d{2})"
+
+# Loop over each file in the directory
+for file_name in os.listdir(directory_path):
+    # Check if the file is a .yang file
+    if file_name.endswith(".yang"):
+        # Get the full file path
+        file_path = os.path.join(directory_path, file_name)
+
+        # Open the file and read its contents
+        with open(file_path, "r") as f:
+            yang_contents = f.read()
+
+        # Find all revision dates within the yang contents
+        matches = re.findall(revision_date_regex, yang_contents)
+        print(file_name, matches)
+
+        # Get the latest revision date
+        latest_revision_date = max(matches) if matches else None
+
+        # If a revision date was found, create the new file name with the revision date
+        if latest_revision_date:
+            # Format the latest revision date as "YYYY-MM-DD"
+            revision_date = time.strptime(latest_revision_date, "%Y-%m-%d")
+            revision_date_str = time.strftime("%Y-%m-%d", revision_date)
+
+            # Create the new file name with the revision date
+            file_name_with_date = f"{os.path.splitext(file_path)[0]}@{revision_date_str}{os.path.splitext(file_path)[1]}"
+
+            # Rename the file with the revision date
+            os.rename(file_path, file_name_with_date)
+
+            # Create a symbolic link to the previous file name
+            previous_file_link = f"{os.path.splitext(file_path)[0]}{os.path.splitext(file_path)[1]}"
+            os.symlink(file_name_with_date, previous_file_link)
