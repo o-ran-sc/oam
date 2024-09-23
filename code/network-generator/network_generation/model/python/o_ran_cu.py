3fc286851f0e9751fa99d5535c658a5fdd3ba5e0
Martin Skorupski <martin.skorupski@highstreet-technologies.com>
2024-09-23 16:31:18 +0200

    Update Copyrights and Python version
    Issue-ID: OAM-414
    Change-Id: I593fd159c891f84ff88c063ae0a9ccf5ad2cae0a
    Signed-off-by: Martin Skorupski <martin.skorupski@highstreet-technologies.com>

--- code/network-generator/network_generation/model/python/o_ran_cu.py
+++ code/network-generator/network_generation/model/python/o_ran_cu.py
@@ -1,4 +1,4 @@
-# Copyright 2023 highstreet technologies GmbH
+# Copyright 2024 highstreet technologies
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# !/usr/bin/python
+# !/usr/bin/python3
 
 """
 A Class representing an O-RAN centralized unit (ORanCu)
