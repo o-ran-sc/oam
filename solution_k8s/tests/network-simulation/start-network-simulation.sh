3304d4de3934a11d24fac5afece8e640f32028f3
Alex Dehn <alexander.dehn@highstreet-technologies.com>
2024-09-26 07:14:30 +0000

    Solution: provide k8s based oam solution
    - single node k8s/kubernetes cluster
    - OAM based on ONAP NewDelhi release
    - performance metric dashboard
    
    Issue-ID: OAM-416
    Change-Id: Icc45a81b528692fcfcc81df3b797e147776dc5a9
    Signed-off-by: Alexander Dehn <alexander.dehn@highstreet-technologies.com>

---
+++ solution_k8s/tests/network-simulation/start-network-simulation.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+# Copyright 2022 highstreet technologies GmbH
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+dep_dir=o-ran-sc-dep-repo
+rm -rf ./$dep_dir
+git clone https://gerrit.o-ran-sc.org/r/it/dep.git $dep_dir
+
+helm cm-push -f $dep_dir/smo-install/tests_oom/ru-simulator local
+helm cm-push -f $dep_dir/smo-install/tests_oom/du-simulator local
+
+helm repo update
+
+helm install --debug oran-ru-simulator local/ru-simulator -n network --create-namespace -f ru-sim.override.yaml
+helm install --debug oran-du-simulator local/du-simulator -n network --create-namespace -f du-sim.override.yaml
